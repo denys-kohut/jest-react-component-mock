4d7015d7b4d0b57fcc4744777cf5085c1eba1f93
Denys Kohut <denys_kohut@epa.com>
2022-05-20 01:00:33 +0300

    fixed string serializations

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 module.exports.mockComponent = (componentName, props) => {
   const defaultTab = '\n      ';
+  const functionTerm = 'function(){ [native code] }'
   const passedProps = Object.keys(props);
+
   const generatedProps = passedProps
     .map(passedProperty => {
       const propValue = props[passedProperty];
-      const value = typeof propValue === 'function'
-        ? 'function(){ [native code] }'
-        : propValue;
+      const value = typeof propValue === 'function' && !propValue.name
+        ? functionTerm
+        : propValue.name || propValue;
+      const parsedValue = typeof value === 'string'
+        ? `'${value}'`
+        : JSON.stringify(value)
 
-      return `${passedProperty}={${JSON.stringify(value)}}`;
+      return `${passedProperty}={${parsedValue === passedProperty ? passedProperty: parsedValue}}`;
     })
     .join(defaultTab);
 
-  return `🚀
+  return `
+  🚀
     ≺${componentName}
       ${generatedProps}
     ∕≻
-  🚀`;
+  🚀
+  `;
 };
